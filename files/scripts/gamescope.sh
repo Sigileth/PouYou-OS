907e8afde8c8df563ed5c79053d63a437d981b23
Sigileth <ankalagon@ryuma.org>
2026-04-10 21:16:34 +0200

    add gamescope.sh

---
+++ files/scripts/gamescope.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'install terra repo'
+dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release{,-extras,-mesa}
+echo 'install gamescope'
+dnf5 -y install \
+    gamescope.x86_64 \
+    gamescope-libs.x86_64 \
+    gamescope-libs.i686 \
+    gamescope-shaders \
+    ScopeBuddy
+echo 'remove terra repo'
+dnf5 -y remove \
+    terra-gpg-keys \
+    terra-release \
+    terra-release-extras \
+    terra-release-mesa
