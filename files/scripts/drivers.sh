0b90c568f395b5032d860861e90f8813f86892ef
Sigileth <ankalagon@ryuma.org>
2026-05-01 21:16:03 +0200

    add drivers

--- files/scripts/drivers.sh
+++ files/scripts/drivers.sh
@@ -12,12 +12,12 @@ QUALIFIED_KERNEL=$(rpm -q --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel
 echo 'install terra repo'
 dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release{,-extras,-mesa}
 echo 'install gamescope from terra repo'
-dnf5 -y install dkms-zenergy dkms
-   # dkms-xone \
-   # dkms-xpadneo \
-   # dkms-xpad-noone \
-   # xone-firmware \
+dnf5 -y install dkms-zenergy dkms dkms-xone akmod-xpadneo dkms-xpad-noone xone-firmware akmod
 
+mkdir -p /var/log/akmods
+touch /var/log/akmods/akmods.log
+KVER="$(dnf5 repoquery --installed --qf '%{VERSION}-%{RELEASE}.%{ARCH}' kernel-cachyos)"
+akmods --force --kernels "$KVER"
 
 # Build all installed DKMS modules for the installed kernel (if any)
 while IFS=' ' read -r pkg_name pkg_ver; do
