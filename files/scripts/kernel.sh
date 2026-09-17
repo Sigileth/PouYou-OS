c08facb38d3e8ebbb7db9d08552774328f8a8dbf
Sigileth <ankalagon@ryuma.org>
2026-09-17 20:25:17 +0200

    change to kernel.sh

--- files/scripts/kernel.sh
+++ files/scripts/kernel.sh
@@ -9,7 +9,7 @@ set -oue pipefail
 
 # Remove Fedora kernel & remove leftover files
 #dnf -y remove kernel kernel-headers kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-tools kernel-tools-libs
-rpm -e --nodeps kernel kernel-headers kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-tools kernel-tools-libs
+rpm -e --nodeps kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-tools kernel-tools-libs
 rm -r -f /usr/lib/modules/*
 
 # Enable repos
